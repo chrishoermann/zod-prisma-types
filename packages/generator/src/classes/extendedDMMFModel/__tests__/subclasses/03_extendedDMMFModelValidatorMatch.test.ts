dc9f6d60380aae297ca70f9ff962bba3b33dff6c
Luke Taylor <luke@deentaylor.com>
2025-04-10 14:38:07 -0400

    Update DMMF import in tests

--- packages/generator/src/classes/extendedDMMFModel/__tests__/subclasses/03_extendedDMMFModelValidatorMatch.test.ts
+++ packages/generator/src/classes/extendedDMMFModel/__tests__/subclasses/03_extendedDMMFModelValidatorMatch.test.ts
@@ -1,4 +1,4 @@
-import { DMMF } from '@prisma/generator-helper';
+import type DMMF from '@prisma/dmmf';
 import { it, expect, describe } from 'vitest';
 
 import { DEFAULT_GENERATOR_CONFIG, MODEL_BASE } from '../setup';
